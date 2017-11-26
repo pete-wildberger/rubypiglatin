7efb8b1ea6fbe388f1ae7641395fb77cea7c042a
Peter Wildberger <pete.wildberger@gmail.com>
2017-11-26 13:57:41 -0600

    running but not working

--- pigLatin.rb
+++ pigLatin.rb
@@ -1,42 +1,43 @@
 #pigLatin.rb
 puts 'Pig Latin'
 
-program_active = True
+program_active = TRUE
+vowels = ['a', 'e', 'i', 'o', 'u']
 
-def pigLatin(original):
-    if len(original) > 0 and original[0] in ('a', 'e', 'i', 'o', 'u') and original.isalpha():
-        word = original.lower()
+def pigLatin(original)
+    if original.length > 0 and vowels.include? original[0] and original.match(/^[[:alpha:]]+$/)
+        word = original.downcase
         new_word = word + pyg
         return new_word
-    elif len(original) > 0 and original.isalpha():
-        word = original.lower()
-        vowel_indices = [idx for idx, ch in enumerate(original) if ch.lower() in 'aeiou']
-        splitIdx = vowel_indices[0]
-        first = original[0 : splitIdx]
-        new_word = word[splitIdx: len(word)] + first + pyg
+    elsif original.length > 0 and original.match(/^[[:alpha:]]+$/)
+        word = original.downcase
+        splitword = word.split(/([aeiou].*)/)
+        new_word = splitword[1] + splitword[0] + pyg
         return new_word
-    else:
+    else
         puts 'empty'
-      end
     end
-  end
+end
 
-while program_active:
+while program_active
     puts 'Type \'quit\' to exit'
     pyg = 'ay'
     puts 'Enter a sentence:'
     STDOUT.flush
     sentence = gets.chomp
-    if sentence == 'quit':
-        program_active = False
+    if sentence == 'quit'
+        program_active = FALSE
+    end
 
     output = []
 
-    sentArr = sentence.split( )
+    sentArr = sentence.split(' ')
 
-    for item in sentArr:
+    for item in sentArr
         output.append(pigLatin(item))
         answer = ' '.join(output)
+      end
     puts answer
     output = []
     sentArr = []
+end
